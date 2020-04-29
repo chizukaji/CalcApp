481f6d544cf44e13194d926186d11c87cbe708f6
chizukaji <too_fine_life@izm.bbiq.jp>
2020-04-29 17:39:48 +0900

    仮完成

--- app/src/main/java/jp/techacademy/chizu/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/chizu/calcapp/MainActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import jp.techacademy.chizu.calcapp.intent.SecondActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,28 +24,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View){
 
+        if(editText1.text.toString() == "" || editText2.text.toString() == ""){
+            val mySnackbar = Snackbar.make(v , "数字を2つ入力してください。", Snackbar.LENGTH_LONG)
+            mySnackbar.show()} else {
 
-        val intent = Intent(this, SecondActivity::class.java)
+            val intent = Intent(this, SecondActivity::class.java)
 
-        val button = when(v.id){
-            R.id.button1 -> 1
-            R.id.button2 -> 2
-            R.id.button3 -> 3
-            else -> 4
+            val button = when (v.id) {
+                R.id.button1 -> 1
+                R.id.button2 -> 2
+                R.id.button3 -> 3
+                else -> 4
+            }
+
+            if (button == 4.toInt()) {
+
+                if (editText2.text.toString() == 0.toString()) {
+                    val mySnackbar = Snackbar.make(v, "0で割ることはできません。", Snackbar.LENGTH_LONG)
+                    mySnackbar.show()
+                } else {
+
+                    intent.putExtra("BUTTON", button.toString())
+                    intent.putExtra("VALUE1", editText1.text.toString())
+                    intent.putExtra("VALUE2", editText2.text.toString())
+
+                    startActivity(intent)
+                }
+
+            } else {
+
+                intent.putExtra("BUTTON", button.toString())
+                intent.putExtra("VALUE1", editText1.text.toString())
+                intent.putExtra("VALUE2", editText2.text.toString())
+
+                startActivity(intent)
+
+            }
         }
-
-        if (button == 4){
-            val mySnackbar = Snackbar.make(view , "Message", Snackbar.LENGTH_LONG)
-            mySnackbar.show()
-        }
-
-        val Value1 = editText1
-        val Value2 = editText2
-
-        intent.putExtra("BUTTON", button.toString())
-        intent.putExtra("VALUE1", Value1.text.toString())
-        intent.putExtra("VALUE2", Value2.text.toString())
-
-        startActivity(intent)
     }
 }
